f658e5bdd153a62e9837f7148f805aedc23965ff
zsjlu <940142834@qq.com>
2021-01-10 19:25:43 +0800

    新增了增删改查，验证成功 简单关系设置没有成功

--- platform_autotest/src/backend.py
+++ platform_autotest/src/backend.py
@@ -31,11 +31,14 @@ app.config['db'] = []
 def hello_world():
     return 'Hello, World!'
 
-class TestCase(db.Model):
+class TestCase_(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(120), nullable=True)
     steps = db.Column(db.String(1024), nullable=True)
+    #
+    # testserver_id = db.Column(db.Integer, db.ForeignKey('testserver_.id'), nullable=True)
+    # testserver = db.relationship('TestService_', backref=db.backref('testcase_', lazy=True))
 
     def __repr__(self):
         return '<TestCase %r>' % self.name
@@ -44,19 +47,41 @@ class TestCaseService(Resource):
     def get(self):
         """
         测试用例的浏览 /testcase.json /testcase.json?id=1
+        查询全部、通过的查询单条、通过负整数删除单条记录
         :return:
         """
-        print(TestCase.query.all())
-        testcases: List[TestCase] = TestCase.query.all()
-        res = [{
-            'id': testcase.id,
-            'name': testcase.name,
-            'description': testcase.description,
-            'steps': json.loads(testcase.steps)
-        } for testcase in testcases]
-        return {
-            'body': res
-        }
+        # 获取参数，通过id筛选
+        req = request.args
+        var_args = req['id'] if "id" in req else ''
+        if var_args:
+            if int(var_args) < 0:
+                var_args = -int(var_args)
+                testcases: List[TestCase_] = TestCase_.query.filter(TestCase_.id == str(var_args)).first()
+                db.session.delete(testcases)
+                db.session.commit()
+            else:
+                testcases: List[TestCase_] = TestCase_.query.filter(TestCase_.id == var_args)
+                res = [{
+                    'id': testcase.id,
+                    'name': testcase.name,
+                    'description': testcase.description,
+                    'steps': json.loads(testcase.steps)
+                } for testcase in testcases]
+                return {
+                    'body': res
+                }
+        else:
+            testcases: List[TestCase_] = TestCase_.query.all()
+            res = [{
+                'id': testcase.id,
+                'name': testcase.name,
+                'description': testcase.description,
+                'steps': json.loads(testcase.steps)
+            } for testcase in testcases]
+            return {
+                'body': res
+            }
+
 
     def post(self):
         """
@@ -64,14 +89,39 @@ class TestCaseService(Resource):
         /testcase.json {'name': 'xx', 'description': 'xxx', 'steps':[]}
         :return:
         """
-        testcase=TestCase(
-            name=request.json.get('name'),
-            description=request.json.get('description'),
-            steps=json.dumps(request.json.get('steps'))
-            )
-        db.session.add(testcase)
-        db.session.commit()
+        req = request.json
+        var_args = req['id'] if "id" in req else ''
+        if var_args:
+            # 更新方法方法一
+            TestCase_.query.filter_by(id=var_args).update({
+                'name': req['name'],
+                'description': req['description'],
+                'steps': json.dumps(req['steps'])
+            })
+            # 更新方法方法二
+            # testcases: List[TestCase] = TestCase.query.filter_by(id=var_args)
+            # testcases.name = req['name']
+            # testcases.description = req['description']
+            # testcases.steps = json.dumps(req['steps']
+            db.session.commit()
+        else:
+            testcase=TestCase_(
+                name=request.json.get('name'),
+                description=request.json.get('description'),
+                steps=json.dumps(request.json.get('steps'))
+                )
+            db.session.add(testcase)
+            db.session.commit()
         return 'ok'
+
+# class TestService_(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     servicename = db.Column(db.String(80), unique=True, nullable=False)
+#     description = db.Column(db.String(120), nullable=True)
+#     steps = db.Column(db.String(1024), nullable=True)
+#
+#     def __repr__(self):
+#         return '<TestService_ %r>' % self.name
 
 class TaskService(Resource):
     def get(self):
--- platform_autotest/tests/test_backend.py
+++ platform_autotest/tests/test_backend.py
@@ -6,19 +6,51 @@ from datetime import datetime
 
 import requests
 
+testcase_url = 'http://127.0.0.1:5000/testcase'
 
+# 查询所有记录
 def test_testcase_get():
-    testcase_url = 'http://127.0.0.1:5000/testcase'
-    r=requests.post(
+
+    r = requests.get(testcase_url)
+    print(r.json())
+    assert r.json()['body']
+
+# 通过id查询某条记录
+def test_testcase_get_by_id():
+
+    r = requests.get(testcase_url, params={'id': '1'})
+    print(r.json())
+    assert r.json()['body']
+
+# 插入新输入，不含id字段表示新增
+def test_testcase_post():
+    r = requests.post(
         testcase_url,
         json={
             'name': f'case{datetime.now().isoformat()}',
             'description': 'description 1',
             'steps': ['1', '2', '3']
         }
-                   )
-    assert  r.status_code == 200
+    )
+    assert r.status_code == 200
 
-    r = requests.get(testcase_url)
-    print(r.json())
-    assert r.json()['body']
+# 更新，json中有id关键字表示更新
+def test_testcase_update():
+    r = requests.post(
+        testcase_url,
+        json={
+            'id': '1',
+            'name': f'case{datetime.now().isoformat()}',
+            'description': 'update',
+            'steps': ['3', '2', '1']
+        }
+    )
+    assert r.status_code == 200
+
+# 删除某条记录，以符号开头表示删除
+def test_testcase_delete():
+    r = requests.get(
+        testcase_url,
+        params={'id': '-16'}
+    )
+    assert r.status_code == 200
